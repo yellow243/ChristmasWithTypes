711389e62a09fcaa48ad9254778c0859b2aae0a3
Zach Padovano <zacharyaz10@gmail.com>
2023-04-07 07:48:23 -0700

    'NewChristmas'

--- ChristmasWithTypes/Program.cs
+++ ChristmasWithTypes/Program.cs
@@ -10,14 +10,14 @@ namespace ChristmasWithTypes
 
             var xmasDay = Christmas.Day.Thursday;
 
-            //TODO set Santa's name to Kris Kringle
-            xmas.Santa = null;
+            //Done set Santa's name to Kris Kringle
+            xmas.Santa = "Kris Kringle";
 
-            //TODO Insert 3 presents you would like for xmas.  They must be strings
-            xmas.Presents =  new string[3]{ }; 
+            //DONE Insert 3 presents you would like for xmas.  They must be strings
+            xmas.Presents =  new string[3]{ "Games", "Fat stacks of money", "Bones for my dogs"   }; 
 
-            //TODO Set the TreeHeight to 10
-            xmas.TreeHeight = null;
+            //DONE Set the TreeHeight to 10
+            xmas.TreeHeight = 10;
 
             Console.WriteLine($"This year christmas falls on {xmasDay} \n");
             Console.WriteLine($"Our tree will be {xmas.TreeHeight} feet high \n");
